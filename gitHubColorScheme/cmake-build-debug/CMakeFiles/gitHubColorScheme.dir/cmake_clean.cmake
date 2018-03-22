93073a19f35d5447e1643a967216d553fb075526
Ryan Chacon <ryan@ryans-mbp.attlocal.net>
2018-03-22 16:01:23 -0700

    creating a github color scheme for c++

---
+++ gitHubColorScheme/cmake-build-debug/CMakeFiles/gitHubColorScheme.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitHubColorScheme.dir/main.cpp.o"
+  "gitHubColorScheme.pdb"
+  "gitHubColorScheme"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitHubColorScheme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
